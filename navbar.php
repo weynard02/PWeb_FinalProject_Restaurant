2921b1486e50eb5380864a48eb7454048ec4bbda
athraz <athrazan2004@gmail.com>
2023-06-09 20:22:48 +0700

    feat: user login and register

--- navbar.php
+++ navbar.php
@@ -24,16 +24,36 @@
       <li class="nav-item">
           <a class="nav-link cart" href="#"><i data-feather="shopping-cart"></i></a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i data-feather="user"></i>&nbsp; Nama User
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Login</a></li>
-            <li><a class="dropdown-item" href="#">Register</a></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
-          </ul>
-        </li>
+        <?php
+          session_start();
+          if (isset($_SESSION['user_id'])){
+            $uid = $_SESSION['user_id'];
+            $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM users WHERE id = '$uid'"));
+            echo '
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i data-feather="user"></i>&nbsp; Hi, '. $user['name'] .
+              '</a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="proses-logout.php">Log out</a></li>
+              </ul>
+            </li>
+            ';
+          }
+          else {
+            echo '
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i data-feather="user"></i>&nbsp; Guest
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="login.php">Login</a></li>
+                <li><a class="dropdown-item" href="register.php">Register</a></li>
+              </ul>
+            </li>
+            ';
+          }
+        ?>
       </ul>
     </div>
   </div>
---
+++ proses-register.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+include('config.php');
+
+if (isset($_POST['register'])){
+    $email = $_POST['email'];
+    if (strpos($email, '@') == true) {
+        $res = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
+        if (mysqli_num_rows($res) === 1){
+            $_SESSION['failed'] = "Email not available!";
+            header('Location: register.php');
+            exit;
+        }
+    } else {
+        $_SESSION['failed'] = "Email Format is incorrect!";
+        header('Location: register.php');
+        exit;
+    }
+
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $number = $_POST['number'];
+
+    if ($password == '' || $name == '' || $address == '' || $number == ''){
+        $_SESSION['failed'] = "Please complete your data!";
+        header('Location: register.php');
+        exit;
+    }
+
+    mysqli_query($conn, "INSERT INTO users VALUES ('', '$name', '$email', '$number', '$password', '$address', 'user')");
+    $_SESSION['sukses'] = "User register suceed! Please login!";
+    header('Location: login.php');
+}
+
+?>
