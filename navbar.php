cd9f318294a83504577739c7bcecb980dc4ede13
weynard02 <samsicoweynard@gmail.com>
2023-06-09 14:33:26 +0700

    fix: navbar

--- navbar.php
+++ navbar.php
@@ -5,12 +5,12 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#about">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Menu</a>
