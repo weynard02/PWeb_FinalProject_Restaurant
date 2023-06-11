a62238f066dcda67b6753a7cbd44d9d33c48a291
weynard02 <samsicoweynard@gmail.com>
2023-06-11 11:56:21 +0700

    fix: login.php

--- input-menu.php
+++ input-menu.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (!isset($_SESSION['user_id'])){
-        header('Location: login-form.php');
+        header('Location: login.php');
         exit;
     }
     include('config.php');
